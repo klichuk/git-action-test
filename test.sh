fd9df913babf4d8f240c5037eee28fbd87b0ac72
klichuk <36610193+klichuk@users.noreply.github.com>
2022-11-17 20:45:05 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env bash
 #test commit 2
 echo "Klichuk Vladyslav"
-=======
-#!/usr/bin/env bash
-
-echo "Klichuk Vladyslav"
->>>>>>> 84a410f6d4b2e1acdda1d574483ddeb601b1f1fd
